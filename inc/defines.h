d4323f1aa8a59d8d6f5ebca53ec4b5c2db0d56be
Jakub Kosiorek <= jakkos69@gmail.com>
2024-06-07 17:58:39 +0200

    added support for multiple points in multiple locations that can be displaed or sth

--- inc/defines.h
+++ inc/defines.h
@@ -4,9 +4,6 @@
 #include <inttypes.h>
 #include <string>
 
-using col32 = uint32_t;
-
-
 ///values 
 #ifdef DEBUG
 constexpr int WIDTH  = 5;
@@ -16,15 +13,31 @@ constexpr int WIDTH  = 800;
 constexpr int HEIGHT = 600;
 #endif
 
+using col32 = uint32_t;
+using canvas = std::array<std::array<col32, WIDTH>, HEIGHT>;
+
 ///colours 
 namespace colour{
    ///let's say that 0xAARRGGBB
-   constexpr col32 black  = 0xFFFFFF;
+   constexpr col32 white  = 0xFFFFFF;
    constexpr col32 red    = 0xFF0000;
    constexpr col32 blue   = 0x0000FF;
    constexpr col32 green  = 0x00FF00;
-   constexpr col32 white  = 0x00000;
+   constexpr col32 black  = 0x00000;
 }
+
+
+struct point{
+   int x;
+   int y;
+   col32 col{colour::black};
+   size_t size{0};
+   point(const int& _x, const int& _y) : x(_x), y(_y){};
+   point(const int& _x, const int& _y, const col32& _col, const size_t& _size) : x(_x), y(_y), col(_col), size(_size){};
+   point() = delete; ///dont want to allow user to create empty point - this is gonna be haounting me later on...
+   point(point&&) = default;
+   point(const point&) = default;
+};
 
 std::string default_path = "/home/jk/Programming/cpp/Vornoi/res/res.ppm";
 std::string path_red = "/home/jk/Programming/cpp/Vornoi/res/res_red.ppm";
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "defines.h"
 #include <array>
 #include <fstream>
+#include <vector>
 
 void stream_to_file(std::ofstream &_file){
    //base case
@@ -10,89 +11,119 @@ void stream_to_file(std::ofstream &_file){
 template <typename T, typename... Args>
 void stream_to_file(std::ofstream &_file, T && _first, Args&&... args){
    std::string temp = std::to_string(_first);
-#ifdef DEBUG
-   //std::cout<< temp << " ";
-#endif
    _file << std::forward<std::string>(temp) << " ";  ///shold there be a space if this is the last recursion call?
    if constexpr (sizeof...(args) > 0){
       stream_to_file(_file, std::forward<Args>(args)...);
    }
    else{
-      // std::cout<<"\n";
-      // _file<<std::endl;
+      _file<<std::endl;
    }
 }
 
-void fill_image(std::array<std::array<col32, WIDTH>, HEIGHT>& _im, const col32& _col){
+void fill_image(canvas& _im, const col32& _col){
 std::cout<< "filling everything with colour: " << std::hex << _col << std::endl;
 for(size_t i = 0; i < HEIGHT; ++i){
       for(size_t j = 0; j < WIDTH; ++j){
          _im[i][j] = _col;
       }
    }
-
-   // for(auto col : _im){
-   //    for(auto val : col){
-   //       val = _col; 
-   //    }
-   // }
 }
 
-void save_image_as_ppm(const std::array<std::array<col32, WIDTH>, HEIGHT>& _im, std::string _path = default_path){  
-   std::ofstream file(_path);
-   //if(!file.is_open()) exit(); ///something horrible happend probably
+void add_point(canvas& _im, const int& _width, const int& _height, const col32& _col = colour::black, const int& _size = 0){
+   if(_width > WIDTH || _width < 0 || _height > HEIGHT || _height < 0) return;
+   _im[_width][_height] = _col;
+   for(int i = 0; i < _size; ++i)
+   {
+      for(int j = 0; j < _size; ++j){
+         auto is_inside = [&](const int& _x, const int& _y)->bool{
+            if(_x > HEIGHT || _x < 0 || _y > HEIGHT || _y < 0) return false;
+            else return true;
+         };
+         //if(_width + i < WIDTH && _height + j < HEIGHT) _im[_width + i][ _height + j] = _col;
+         if(is_inside(_width + i, _height + j))_im[_width + i][ _height + j] = _col;
+         if(is_inside(_width - i, _height + j))_im[_width - i][ _height + j] = _col;
+         if(is_inside(_width + i, _height - j))_im[_width + i][ _height - j] = _col;
+         if(is_inside(_width - i, _height - j))_im[_width - i][ _height - j] = _col;
+      }
+   }
+}
 
-   // std::string width  = std::to_string(WIDTH);
-   // std::string height = std::to_string(HEIGHT);
+void add_point(canvas& _im, const point& _point)
+{
+   _im[_point.x][_point.y] = _point.col;
+   for(int i = 0; i < _point.size; ++i)
+   {
+      for(int j = 0; j < _point.size; ++j){
+
+         auto is_inside = [&](const int& _x, const int& _y)->bool{
+            if(_x > HEIGHT || _x < 0 || _y > HEIGHT || _y < 0) return false;
+            else return true;
+         };
+         //if(_width + i < WIDTH && _height + j < HEIGHT) _im[_width + i][ _height + j] = _col;
+         if(is_inside(_point.x + i, _point.y + j))_im[_point.x + i][ _point.y + j] = _point.col;
+         if(is_inside(_point.x - i, _point.y + j))_im[_point.x - i][ _point.y + j] = _point.col;
+         if(is_inside(_point.x + i, _point.y - j))_im[_point.x + i][ _point.y - j] = _point.col;
+         if(is_inside(_point.x - i, _point.y - j))_im[_point.x - i][ _point.y - j] = _point.col;
+      }
+   }
+}
+
+void save_image_as_ppm(const canvas& _im, std::string _path = default_path){  
+   std::ofstream file(_path);
    file<<"P3\n"<< std::to_string(WIDTH)<<" "<< std::to_string(HEIGHT)<<std::endl;
    file<<255<<std::endl;
    std::cout<<std::dec;
-#ifdef DEBUG
-   std::cout<<"P3\n"<< std::to_string(WIDTH)<<" "<< std::to_string(HEIGHT)<<std::endl;
-   std::cout<<255<<std::endl<<255<<std::endl;
-#endif
    for(auto col : _im)
    {
       for(col32 val : col)
       {
          ///val saved as RRR GGG BBB where each of those is a number from 0 - 255. next line in next pixel
-         //uint8_t red    = val & 0xFF << 8*2;
-         
-         // uint8_t red    =  val & (0xFF << 8*2);
-         // uint8_t green  = val & (0xFF << 8*1);
-         // uint8_t blue   = val & (0xFF << 8*0);
 
          uint8_t red    =  (val >> 8*2) & 0xFF;
          uint8_t green  =  (val >> 8*1) & 0xFF;
          uint8_t blue   =  (val >> 8*0) & 0xFF;
          stream_to_file(file, red, green, blue);
-#ifdef DEBUG
-         // std::cout<<std::hex<<"this is the value of val: \n"<<val<<std::endl;
-         // std::cout<<std::hex<<"this is the value shifted by 16 to the left of val: \n"<<((val & 0xFF0000) >> 8 * 2)<<std::endl;
-         // printf("this is the value of val: %d \n", val);
-         printf("red: %d, green: %d, blue: %d \n", red, green, blue);
-#endif 
       }
       file<<"\n";
-#ifdef DEBUG
-      std::cout<<std::endl;
-#endif 
    }
    file.close();
 }
 
 int main(void){
 
-   std::array<std::array<col32, WIDTH>, HEIGHT> image_red;
-   std::array<std::array<col32, WIDTH>, HEIGHT> image_blue;
-   std::array<std::array<col32, WIDTH>, HEIGHT> image_green;
+   std::vector<point> points;
+
+   canvas image_red;
+   canvas image_blue;
+   canvas image_green;
+
    fill_image(image_red, colour::red);
    fill_image(image_blue, colour::blue);
    fill_image(image_green, colour::green);
 
+
    save_image_as_ppm(image_red, path_red);
    save_image_as_ppm(image_blue, path_blue);
    save_image_as_ppm(image_green, path_green);
+
+   ///testing points here =3
+
+   canvas image_black;
+   fill_image(image_black, colour::black);
+   add_point(image_black, HEIGHT/2, WIDTH/2, colour::white, 3);
+
+   point p1(600, 500, colour::red, 7);
+   point p2(250, 300, colour::blue, 5);
+   points.push_back(p1);
+   points.push_back(p2);
+
+   for(auto p : points)
+   {
+      add_point(image_black, p);
+   };
+
+   add_point(image_black, HEIGHT/2, WIDTH/2, colour::white, 3);
+   save_image_as_ppm(image_black);
    std::cout<<"i'm still working UwU, but barely...\n";
    return 0;
 }
